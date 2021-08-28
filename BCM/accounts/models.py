a19a9c72e422509ce76a219efa997ce334b0a56d
Shin-Sae-byeok <toqur1219@naver.com>
2021-08-28 10:11:39 +0900

    feat:final user data

--- BCM/accounts/models.py
+++ BCM/accounts/models.py
@@ -6,11 +6,13 @@ class Users(AbstractUser):
     GENDER_CHOICES = (
         ('M', 'Male'),
         ('F', 'Female'),
+        ('C', 'Custom'),
     )
 
     username = models.CharField(max_length=20, verbose_name="이름")
-    # age = models.IntegerField(blank=True, verbose_name="나이")
+    age = models.IntegerField(blank=True, verbose_name="나이")
     sex = models.CharField(max_length=1, blank=True, choices=GENDER_CHOICES, verbose_name="남성 / 여성")
+    bio = models.CharField(max_length=255, verbose_name="자기소개")
     userid = models.CharField(max_length=10, verbose_name="아이디")
     password = models.CharField(max_length=50, verbose_name="비밀번호")
     email = models.EmailField(max_length=254, unique=True, verbose_name="이메일")
